f6589abddd4175a78a572752065466b88b271ed2
vishan <Vishanchathuranga2003@gmail.com>
2025-05-03 21:00:55 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+//Syntax
+let myName: string = "vishan"; //String value
+let myNumber: number = 2; //Number value
+let myBoolean: boolean = true; //Boolean value
+
+let myUnderFineVar: undefined = undefined; //undefined value
+let myNulls: null = null; //null value
+console.log(myName)
+console.log(myNumber)
+console.log(myBoolean)
+console.log(myUnderFineVar)
+console.log(myNulls)
+
+
